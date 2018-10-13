c132d63aa0a9b7aaa02c0a7f8e099529c82f0db9
ned <ned@github>
2018-10-13 20:08:07 +0200

    highlighting text while typing & added search icon

--- appdb/SuggestionsWhileTyping.swift
+++ appdb/SuggestionsWhileTyping.swift
@@ -24,11 +24,9 @@ class SuggestionsWhileTyping: UITableViewController, UISearchResultsUpdating {
     
     var type: ItemType = .ios
     
-    lazy var bgColorView: UIView = {
-        let view = UIView()
-        view.theme_backgroundColor = Color.cellSelectionColor
-        return view
-    }()
+    var matchingColor: UIColor {
+        return Themes.isNight ? .white : .black
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,8 +35,9 @@ class SuggestionsWhileTyping: UITableViewController, UISearchResultsUpdating {
         tableView.theme_backgroundColor = Color.veryVeryLightGray
         view.theme_backgroundColor = Color.veryVeryLightGray
         tableView.theme_separatorColor = Color.borderColor
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "suggestion")
+        tableView.register(SearchSuggestionCell.self, forCellReuseIdentifier: "suggestion")
         tableView.cellLayoutMarginsFollowReadableWidth = true
+        tableView.rowHeight = 40~~35
     }
     
     func updateSearchResults(for searchController: UISearchController) {
@@ -46,7 +45,7 @@ class SuggestionsWhileTyping: UITableViewController, UISearchResultsUpdating {
         if text.count > 1 {
             self.text = text
             NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(self.reload), object: nil)
-            self.perform(#selector(self.reload), with: nil, afterDelay: 0.25)
+            self.perform(#selector(self.reload), with: nil, afterDelay: 0.15)
         } else {
             if results.isEmpty {
                 results = []
@@ -59,7 +58,7 @@ class SuggestionsWhileTyping: UITableViewController, UISearchResultsUpdating {
         API.fastSearch(type: self.type, query: self.text, maxResults: 7, success: { results in
             self.results = results
             self.tableView.reloadData()
-        }, fail: { _ in })
+        })
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -71,22 +70,80 @@ class SuggestionsWhileTyping: UITableViewController, UISearchResultsUpdating {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "suggestion", for: indexPath)
-        cell.contentView.theme_backgroundColor = Color.veryVeryLightGray
-        cell.theme_backgroundColor = Color.veryVeryLightGray
-        cell.textLabel?.theme_textColor = Color.title
+        let cell = tableView.dequeueReusableCell(withIdentifier: "suggestion", for: indexPath) as! SearchSuggestionCell
         if results.indices.contains(indexPath.row) {
             var result = results[indexPath.row]
             while result.hasPrefix(" ") { result = String(result.dropFirst()) }
-            cell.textLabel?.text = result
+            cell.label.attributedText = attributedText(withString: result, matchString: text, font: cell.label.font)
         }
-        cell.selectedBackgroundView = bgColorView
-        //cell.highlight(text: text, normal: nil, highlight: [NSBackgroundColorAttributeName: .yellow], type: UILabel.self)
-        return cell
+       return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         searcherDelegate?.didClickSuggestion(results[indexPath.row])
+    }
+    
+    fileprivate func attributedText(withString string: String, matchString: String, font: UIFont) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: string, attributes: [NSAttributedString.Key.font: font])
+        let boldFontAttribute: [NSAttributedString.Key: Any] = [NSAttributedString.Key.foregroundColor: matchingColor]
+        let range = (string as NSString).range(of: matchString, options: .caseInsensitive)
+        attributedString.addAttributes(boldFontAttribute, range: range)
+        return attributedString
+    }
+}
+
+class SearchSuggestionCell: UITableViewCell {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    lazy var bgColorView: UIView = {
+        let view = UIView()
+        view.theme_backgroundColor = Color.cellSelectionColor
+        return view
+    }()
+    
+    lazy var searchImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "Search").withRenderingMode(.alwaysTemplate)
+        imageView.theme_tintColor = ["#c6c6c6", "#7c7c7c"]
+        return imageView
+    }()
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: (17~~16))
+        label.numberOfLines = 1
+        label.theme_textColor = ["#777777", "#828282"]
+        label.makeDynamicFont()
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.theme_backgroundColor = Color.veryVeryLightGray
+        theme_backgroundColor = Color.veryVeryLightGray
+        textLabel?.theme_textColor = Color.title
+        selectedBackgroundView = bgColorView
+        
+        contentView.addSubview(label)
+        contentView.addSubview(searchImageView)
+        
+        constrain(label, searchImageView) { label, image in
+            
+            image.height == 20
+            image.width == 30
+            
+            image.left == image.superview!.layoutMarginsGuide.left
+            image.centerY == image.superview!.centerY
+            
+            label.left == image.right + (10~~7)
+            label.right == label.superview!.layoutMarginsGuide.right
+            label.centerY == image.centerY
+        }
+        
     }
 }
