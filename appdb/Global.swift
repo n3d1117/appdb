f67a74feaae964317895ae12b60f12acc1917d7b
ned <ned@git>
2019-04-25 17:27:27 +0200

    some additions

--- appdb/Global.swift
+++ appdb/Global.swift
@@ -129,4 +129,15 @@ struct Global {
         return "now".localized()
     }
     
+    // Returns true if given date is max 60 seconds away from now
+    static func isSecondsAway(from startDate: Date) -> Bool {
+        let calendar = Calendar.current
+        let difference = calendar.dateComponents([.day, .hour, .minute, .second], from: startDate, to: Date())
+        
+        guard let s = difference.second, let m = difference.minute, let h = difference.hour, let d = difference.day else { return false }
+
+        if d > 0 || h > 0 || m > 0 { return false }
+        return s > 0
+    }
+    
 }
