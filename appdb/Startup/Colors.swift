c184a1757f078a1e4e2a28f35f2716ff7ac8070f
ned <ned@git>
2019-06-04 16:34:37 +0200

    small color & image fixes

--- appdb/Startup/Colors.swift
+++ appdb/Startup/Colors.swift
@@ -33,7 +33,7 @@ enum Color {
     static let tableViewBackgroundColor: ThemeColorPicker = ["#EFEFF4", "#121212", "#000000"]
 
     /* TableView separator color */
-    static let borderColor: ThemeColorPicker = ["#C7C7CC", "#373737", "#373737"]
+    static let borderColor: ThemeColorPicker = ["#C7C7CC", "#373737", "#000000"]
 
     /* Error message, copyright text */
     static let copyrightText: ThemeColorPicker = ["#555555", "#7E7E7E", "#7E7E7E"]
@@ -48,7 +48,7 @@ enum Color {
     static let invertedTitle: ThemeColorPicker = ["#F8F8F8", "#121212", "#121212"]
 
     /* Cell selection overlay color */
-    static let cellSelectionColor: ThemeColorPicker = ["#D8D8D8", "#383838", "#383838"]
+    static let cellSelectionColor: ThemeColorPicker = ["#D8D8D8", "#383838", "#111111"]
 
     /* Matches translucent barStyle color */
     static let popoverArrowColor: ThemeColorPicker = ["#F6F6F7", "#161616", "#161616"]
@@ -103,10 +103,10 @@ enum Color {
     static let copyrightTextCgColor = ThemeCGColorPicker(colors: "#555555", "#7E7E7E", "#7E7E7E")
 
     /* Icon layer borderColor */
-    static let borderCgColor = ThemeCGColorPicker(colors: "#C7C7CC", "#1E1E1E", "#1E1E1E")
+    static let borderCgColor = ThemeCGColorPicker(colors: "#C7C7CC", "#373737", "#000000")
 
     /* CG version of tableViewBackgroundColor */
-    static let tableViewCGBackgroundColor = ThemeCGColorPicker(colors: "#EFEFF4", "#121212", "#121212")
+    static let tableViewCGBackgroundColor = ThemeCGColorPicker(colors: "#EFEFF4", "#121212", "#000000")
 
     /* Hardcoded Apple's UIButton selected color */
     static let buttonBorderCgColor = ThemeCGColorPicker(colors: "#D0D0D4", "#272727", "#272727")
