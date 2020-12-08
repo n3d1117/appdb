43d9d4ef7cf9aebc9856ce280b23280b0a0e10bf
ned <ned@mbp.local>
2020-12-08 13:58:59 +0100

    update news APIs

--- appdb/API/API+News.swift
+++ appdb/API/API+News.swift
@@ -12,7 +12,7 @@ import Alamofire
 extension API {
 
     static func getNews(limit: Int = 10, success:@escaping (_ items: [SingleNews]) -> Void, fail:@escaping (_ error: NSError) -> Void) {
-        AF.request(endpoint, parameters: ["action": Actions.getNews.rawValue, "lang": languageCode, "limit": String(limit)], headers: headers)
+        AF.request(endpoint, parameters: ["action": Actions.getPages.rawValue, "category": Actions.newsCategory.rawValue, "lang": languageCode, "length": String(limit)], headers: headers)
             .responseArray(keyPath: "data") { (response: AFDataResponse<[SingleNews]>) in
                 switch response.result {
                 case .success(let news):
@@ -24,7 +24,7 @@ extension API {
     }
 
     static func getNewsDetail(id: String, success:@escaping (_ item: SingleNews) -> Void, fail:@escaping (_ error: NSError) -> Void) {
-        AF.request(endpoint, parameters: ["action": Actions.getNews.rawValue, "lang": languageCode, "id": id], headers: headers)
+        AF.request(endpoint, parameters: ["action": Actions.getPages.rawValue, "category": Actions.newsCategory.rawValue, "lang": languageCode, "id": id], headers: headers)
         .responseObject(keyPath: "data") { (response: AFDataResponse<SingleNews>) in
             switch response.result {
             case .success(let singleNews):
